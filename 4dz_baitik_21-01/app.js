9c36b13c9e36e8a5cfbff0c20886e02ca73ac5b0
BaiTikk <120124623+BaiTikk@users.noreply.github.com>
2023-01-25 22:55:13 +0600

    !&&

---
+++ 4dz_baitik_21-01/app.js
@@ -0,0 +1,23 @@
+let button = document.querySelector(".btn")
+let items = document.querySelector(".items")
+
+button.onclick = () => {
+    const request = new XMLHttpRequest()
+    request.open("GET", "son.json")
+    request.setRequestHeader("Content-type", "application/json")
+    request.send()
+    request.addEventListener("load", () => {
+        const data = JSON.parse(request.response)
+        data.forEach(item => {
+            const div = document.createElement('div')
+            div.innerHTML = `
+               <div class="card">
+                    <img src="${item.img}">
+                    <h2>${item.name}</h2>
+                    <span>${item.price}</span>
+               </div>
+               `
+            items.append(div)
+        })
+    })
+}
